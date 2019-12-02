5f5c045cf4b2773730a9cb6e8fb76ccbe1c21783
Nikhil Bhargava <nikhil.bhargava.15@utexas.edu>
2019-12-01 23:12:01 -0600

    adding cleaning node

---
+++ src/sneakairs/nodes/kof_clean.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+import requests
+import urllib.request
+import time
+from bs4 import BeautifulSoup
+from sneakairs.nodes.kof_functions import p_get_data, meta_get_data
+
+def clean(*args):
+
